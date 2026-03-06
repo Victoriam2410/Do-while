7adbcf865471c63d854ded6c5d6488e5463a6660
Victoriam2410 <estherrodas61@gmail.com>
2026-03-05 23:33:51 -0600

    Add files via upload

---
+++ do While/RodasEsther_Do19/RodasEsther_Do19/Program.cs
@@ -0,0 +1,11 @@
+﻿int numero;
+int contador = 1;
+
+Console.WriteLine("Ingrese un número para ver su tabla:");
+numero = int.Parse(Console.ReadLine());
+
+do
+{
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+    contador++;
+} while (contador <= 10);
