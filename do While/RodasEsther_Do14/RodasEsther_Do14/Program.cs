f5f5c144222c20f018c569b838500ae2f4b43f3c
Victoriam2410 <estherrodas61@gmail.com>
2026-03-05 23:22:21 -0600

    Add files via upload

---
+++ do While/RodasEsther_Do14/RodasEsther_Do14/Program.cs
@@ -0,0 +1,18 @@
+﻿int numero;
+int mayor;
+
+Console.WriteLine("ingrese cualquier numero (0 para detener)");
+numero = int.Parse(Console.ReadLine());
+mayor = numero;
+
+while (numero != 0)
+{
+    if (numero > mayor)
+    {
+        mayor = numero;
+    }
+    Console.WriteLine("ingrese numero");
+    numero = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"El número mayor es: {mayor}");
