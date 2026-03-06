e1c79191c74e996768a25bc13cf79e5d6f9eceb1
Victoriam2410 <estherrodas61@gmail.com>
2026-03-05 23:00:39 -0600

    Add files via upload

---
+++ do While/RodasEsther_Do11/RodasEsther_Do11/Program.cs
@@ -0,0 +1,11 @@
+﻿int numero;
+int suma = 0;
+int x = 1;
+do
+{
+    Console.WriteLine("ingrese numero");
+    numero = int.Parse(Console.ReadLine());
+    suma += numero;
+    x++;    
+} while ( x <= 5);
+Console.WriteLine($"la suma de los numeros es : {suma}");
