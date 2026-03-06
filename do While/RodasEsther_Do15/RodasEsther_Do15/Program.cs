a30968039cf311afb5db40da5686e238436a20ad
Victoriam2410 <estherrodas61@gmail.com>
2026-03-05 23:25:16 -0600

    Add files via upload

---
+++ do While/RodasEsther_Do15/RodasEsther_Do15/Program.cs
@@ -0,0 +1,9 @@
+﻿int numero;
+int x = 0;
+do
+{
+    Console.WriteLine("ingrese numero positivo");
+    numero = int.Parse(Console.ReadLine());
+    x++;
+} while (numero < 0);
+Console.WriteLine("numero valido");
