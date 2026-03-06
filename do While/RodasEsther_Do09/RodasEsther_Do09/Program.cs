ae27fa0dba9614f6433dfa9e24e171ced01e00ab
Victoriam2410 <estherrodas61@gmail.com>
2026-03-05 22:45:55 -0600

    Add files via upload

---
+++ do While/RodasEsther_Do09/RodasEsther_Do09/Program.cs
@@ -0,0 +1,13 @@
+﻿int numero = 1;
+do
+{
+    if (numero % 2==0)
+    {
+        Console.WriteLine($"{numero} es par");
+    }
+    else
+    {
+        Console.WriteLine($"{numero} es impar");
+    }
+    numero++;
+} while (numero <= 20);
