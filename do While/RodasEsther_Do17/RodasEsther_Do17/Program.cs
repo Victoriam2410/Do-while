e21d13fe5facd9ba7a322544b8bfd5000695c8f5
Victoriam2410 <estherrodas61@gmail.com>
2026-03-05 23:30:26 -0600

    Add files via upload

---
+++ do While/RodasEsther_Do17/RodasEsther_Do17/Program.cs
@@ -0,0 +1,18 @@
+﻿int numero;
+int pares = 0;
+
+Console.WriteLine("ingrese números (0 para detener)");
+
+do
+{
+    Console.WriteLine("ingrese número");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero != 0 && numero % 2 == 0)
+    {
+        pares++;
+    }
+
+} while (numero != 0);
+
+Console.WriteLine($"Cantidad de números pares: {pares}");
