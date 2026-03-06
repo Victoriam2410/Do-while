96234d9ee409634ea0ecd965036424bab25512d2
Victoriam2410 <estherrodas61@gmail.com>
2026-03-05 23:35:41 -0600

    Add files via upload

---
+++ do While/RodasEsther_Do20/RodasEsther_Do20/Program.cs
@@ -0,0 +1,28 @@
+﻿int numero;
+double suma = 0;
+int cantidad = 0;
+double promedio = 0;
+
+Console.WriteLine("ingrese números (0 para detener)");
+
+do
+{
+    Console.WriteLine("ingrese numero");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero != 0)
+    {
+        suma = suma + numero;
+        cantidad = cantidad + 1;
+    }
+
+} while (numero != 0);
+
+if (cantidad > 0)
+{
+    promedio = suma / cantidad;
+}
+
+Console.WriteLine($"suma total: {suma}");
+Console.WriteLine($"cantidad de números: {cantidad}");
+Console.WriteLine($"promedio: {promedio}");
