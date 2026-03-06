4e88f246c7e3d0aea865a0341b68b668b8c0e9e1
Victoriam2410 <estherrodas61@gmail.com>
2026-03-05 23:17:26 -0600

    Add files via upload

---
+++ do While/RodasEsther_Do13/RodasEsther_Do13/Program.cs
@@ -0,0 +1,10 @@
+﻿int numero;
+int x = 0;
+Console.WriteLine("ingrese cualquier numero (0 para detener)");
+do
+{
+    Console.WriteLine("ingrese numero");
+    numero = int.Parse(Console.ReadLine());
+    x++;
+} while (numero != 0);
+Console.WriteLine($"los numeros que ingreso son: {x}");
