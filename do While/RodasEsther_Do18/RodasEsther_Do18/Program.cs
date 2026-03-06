2bdd36620faf42982a4fa9904ab5667ec3d34412
Victoriam2410 <estherrodas61@gmail.com>
2026-03-05 23:32:49 -0600

    Add files via upload

---
+++ do While/RodasEsther_Do18/RodasEsther_Do18/Program.cs
@@ -0,0 +1,20 @@
+﻿int opcion;
+
+do
+{
+    Console.WriteLine("1 Mostrar saludo");
+    Console.WriteLine("2 Mostrar número aleatorio");
+    Console.WriteLine("0 Salir");
+
+    opcion = int.Parse(Console.ReadLine());
+
+    if (opcion == 1)
+    {
+        Console.WriteLine("Hola, bienvenido.");
+    }
+    if (opcion == 2)
+    {
+        Console.WriteLine("Número generado: 42");
+    }
+
+} while (opcion != 0);
