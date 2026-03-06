dc0875f8a4d78d28093bc79529294eca0d50c7d7
Victoriam2410 <estherrodas61@gmail.com>
2026-03-05 23:28:36 -0600

    Add files via upload

---
+++ do While/RodasEsther_Do16/RodasEsther_Do16/Program.cs
@@ -0,0 +1,9 @@
+﻿string pass;
+int x = 0;
+do
+{
+    Console.WriteLine("ingrese contraseña");
+    pass = Console.ReadLine();
+    x++;
+} while (pass != "2410");
+Console.WriteLine("contraseña correcta");
